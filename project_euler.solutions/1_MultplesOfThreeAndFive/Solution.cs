912812889f1c22a086bf9a900694eec01c03ac75
Paul Davis <davis.pauljohn@gmail.com>
2021-06-12 22:58:26 +1000

    Remove test project and define tests with the solution

--- project_euler.solutions/1_MultplesOfThreeAndFive/Solution.cs
+++ project_euler.solutions/1_MultplesOfThreeAndFive/Solution.cs
@@ -2,13 +2,15 @@
 
 namespace project_euler.solutions
 {
-    /// <summary>
-    /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-    /// Find the sum of all the multiples of 3 or 5 below 1000.
-    /// </summary>
     public class MultiplesOfThreeAndFive
     {
-        public static int Run(int upperLimit)
+        /// <summary>
+        /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+        /// Find the sum of all the multiples of 3 or 5 below 1000.
+        /// </summary>
+        /// <param name="upperLimit">The exclusive upper limit</param>
+        /// <returns>The sum of the multiples of three and five between 0 and a user specified limit</returns>
+        public static int GetMultiplesOfThreeAndFive(int upperLimit)
         {
             int count = 1;
             int sum = 0;
