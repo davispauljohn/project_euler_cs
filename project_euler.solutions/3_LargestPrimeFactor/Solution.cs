886870c667ac65f89e18ccc8d3d0301b918fd8ce
Paul Davis <davis.pauljohn@gmail.com>
2021-06-13 15:23:23 +1000

    3. Largest Prime Factor

---
+++ project_euler.solutions/3_LargestPrimeFactor/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace project_euler.solutions
+{
+    public class LargestPrimeFactor
+    {
+        /// <summary>
+        /// The prime factors of 13195 are 5, 7, 13 and 29.
+        /// What is the largest prime factor of the number 600851475143 ?
+        /// </summary>
+        /// <param name="subject">The value to find prime factor of</param>
+        /// <returns>The largest prime factor of the subject</returns>
+        public static long GetLargestPrimeFactor(long subject)
+        {
+            if (subject < 2)
+                return 0;
+
+            if (subject % 2 == 0)
+                return subject == 2 ? 0 : 2;
+
+            long largestPrimeDivisor = 0;
+
+            for (var divisor = 3; divisor <= Math.Sqrt(subject); divisor += 2)
+                if (subject % divisor == 0)
+                {
+                    var quotient = subject / divisor;
+                    if (IsPrime(quotient))
+                        return quotient;
+                    if (IsPrime(divisor))
+                        largestPrimeDivisor = divisor;
+                }
+
+            return largestPrimeDivisor;
+        }
+
+        public static bool IsPrime(long number)
+        {
+            if (number < 2)
+                return false;
+
+            if (number % 2 == 0)
+                return number == 2 ? true : false;
+
+            for (int divisor = 3; divisor <= Math.Sqrt(number); divisor += 2)
+                if (number % divisor == 0)
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ project_euler.solutions/3_LargestPrimeFactor/Tests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using project_euler.solutions;
+using Shouldly;
+
+namespace project_euler.tests
+{
+    /// <summary>
+    /// The prime factors of 13195 are 5, 7, 13 and 29.
+    /// What is the largest prime factor of the number 600851475143 ?
+    /// </summary>
+    [TestFixture]
+    public class LargestPrimeFactorTests
+    {
+        [TestCase(0, false)]
+        [TestCase(1, false)]
+        [TestCase(2, true)]
+        [TestCase(3, true)]
+        [TestCase(4, false)]
+        [TestCase(5, true)]
+        public void IsPrime_ShouldReturnExpected(int subject, bool expected)
+        {
+            LargestPrimeFactor.IsPrime(subject).ShouldBe(expected);
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        public void GetPrimeFactors_WhenSubjectIsLessThan2_ShouldReturnEmpty(int subject)
+        {
+            LargestPrimeFactor.GetLargestPrimeFactor(subject).ShouldBe(0);
+        }
+
+        [Test]
+        public void GetPrimeFactors_WhenSubjectIs2_ShouldReturnExpected()
+        {
+            LargestPrimeFactor.GetLargestPrimeFactor(2).ShouldBe(0);
+        }
+
+        [Test]
+        public void GetPrimeFactors_WhenSubjectIs4_ShouldReturnExpected()
+        {
+            LargestPrimeFactor.GetLargestPrimeFactor(4).ShouldBe(2);
+        }
+
+        [Test]
+        public void GetPrimeFactors_WhenSubjectIs15_ShouldReturnExpected()
+        {
+            LargestPrimeFactor.GetLargestPrimeFactor(15).ShouldBe(5);
+        }
+
+        [Test]
+        public void GetPrimeFactors_WhenSubjectIs13195_ShouldReturnExpected()
+        {
+            LargestPrimeFactor.GetLargestPrimeFactor(13195).ShouldBe(29);
+        }
+
+        [Test]
+        public void Solution()
+        {
+            LargestPrimeFactor.GetLargestPrimeFactor(600851475143).ShouldBe(6857);
+        }
+    }
+}
